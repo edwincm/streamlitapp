cd6ebb48bc146e0f77f7c48bc89927eb92827a4e
Edwin Charles Mathew <charlymk56@gmail.com>
2024-12-21 22:36:07 +0530

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from os import path
+import pickle
 
 df = pd.read_csv(path.join("data", "auto-mpg.csv"))
 
@@ -15,3 +16,7 @@ st.scatter_chart(df[["mpg", "weight"]])
 
 x = st.slider("x", min_value=0, max_value=100, value=25)
 st.title("{} squared is {}".format(x, x * x))
+
+st.title("Flower Classification")
+model = pickle.load(open(path.join("model", "lr_model.pkl"), "rb"))
+st.write(model.predict([[1,2,3,4]]))
